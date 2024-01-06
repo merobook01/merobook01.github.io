87f2dbc0907dbbeb9a23ea83c38b1db9bb77195e
dilli <dillihangrae@gmail.com>
2024-01-06 21:32:34 +0545

    committed

--- script.js
+++
@@ -1,6 +0,0 @@
-//dynamically update the date year in footer
-var d = new Date();
-document.getElementById("yearFooter").innerHTML = d.getFullYear();
-
-
-
---
+++ script/chunk/date.js
@@ -0,0 +1,6 @@
+//dynamically update the date year in footer
+var d = new Date();
+document.getElementById("yearFooter").innerHTML = d.getFullYear();
+
+
+
